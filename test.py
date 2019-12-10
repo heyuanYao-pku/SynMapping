a78c42c4e54e5cec2c1a46750d54d9b8e906b10f
16000 <18811711520@163.com>
2019-12-11 00:23:18 +0800

    multi_sol and multi_round but not work well on wolf and hands

--- test.py
+++ test.py
@@ -6,21 +6,22 @@ import TensorMap
 data_path = 'example\\data.json'
 data = json.load(open( data_path,'r') )
 print(data.keys())
-mList = data['mlist'][:35]
+mList = data['mlist']
 tmp = data['P']
 image_list_path = 'example\\image_list.npy'
 image_list = np.load(image_list_path)
 
 #mList = mList[0:20]
-#Plist = np.array(Plist)
-#Plist = Plist[0:20,0:20]
+#tmp = np.array(tmp)
+#tmp = tmp[0:20,0:20]
 #image_list = image_list[0:20]
 
 #print(  np.shape(Plist)  )
 n = np.size(mList)
 Plist = np.zeros(np.shape(tmp), np.ndarray)
 
-
+print(tmp[13][14])
+#exit(0)
 print(  n  )
 #print(mList[0],mList[100],mList[2],mList[102] )
 
@@ -33,15 +34,14 @@ for i in range(n):
 
 
 tensor = TensorMap.SynTensorMap(n,mList,Plist)
-Q = tensor.solution()
-np.save('example\\Q.npy',Q)
-Q = tensor.rounded_solution(0.5,Q)
-
+Q = tensor.rounded_solution(th=0.5,k=1)
+Q = tensor.multi_round(th = 0.5)
 #Q = np.load('example\\Q.npy')
 
 
-print(mList[0:2],Q)
-
+#print(mList[0:2],Q)
+for i in range(len(Q)):
+    print(i, Q[i])
 
 
 draw = TensorMap.TensorMapVis(image_list,mList,Q).draw_image
